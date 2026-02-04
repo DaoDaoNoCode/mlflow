936579255cb0ab6706f44bd871053415b460d1a8
Juntao Wang <juntwang@redhat.com>
2026-02-04 13:16:58 -0500

    Add the support to develop the UI on a live cluster

--- mlflow/server/js/src/setupProxy.js
+++ mlflow/server/js/src/setupProxy.js
@@ -10,36 +10,24 @@ module.exports = function (app) {
   const proxyTarget = process.env.MLFLOW_PROXY || 'http://localhost:5000/';
   // eslint-disable-next-line no-undef
   const proxyStaticTarget = process.env.MLFLOW_STATIC_PROXY || proxyTarget;
-  app.use(
-    createProxyMiddleware('/ajax-api', {
-      target: proxyTarget,
-      changeOrigin: true,
+  // eslint-disable-next-line no-undef
+  const proxyAuthToken = process.env.MLFLOW_PROXY_AUTH_TOKEN;
+
+  const proxyOptions = {
+    changeOrigin: true,
+    secure: false,
+    ...(proxyAuthToken && {
+      onProxyReq: (proxyReq) => {
+        proxyReq.setHeader('Authorization', `Bearer ${proxyAuthToken}`);
+      },
     }),
-  );
+  };
+
+  app.use(createProxyMiddleware('/ajax-api', { target: proxyTarget, ...proxyOptions }));
+  app.use(createProxyMiddleware('/graphql', { target: proxyTarget, ...proxyOptions }));
+  app.use(createProxyMiddleware('/get-artifact', { target: proxyStaticTarget, ws: true, ...proxyOptions }));
   app.use(
-    createProxyMiddleware('/graphql', {
-      target: proxyTarget,
-      changeOrigin: true,
-    }),
+    createProxyMiddleware('/model-versions/get-artifact', { target: proxyStaticTarget, ws: true, ...proxyOptions }),
   );
-  app.use(
-    createProxyMiddleware('/get-artifact', {
-      target: proxyStaticTarget,
-      ws: true,
-      changeOrigin: true,
-    }),
-  );
-  app.use(
-    createProxyMiddleware('/model-versions/get-artifact', {
-      target: proxyStaticTarget,
-      ws: true,
-      changeOrigin: true,
-    }),
-  );
-  app.use(
-    createProxyMiddleware('/gateway', {
-      target: proxyTarget,
-      changeOrigin: true,
-    }),
-  );
+  app.use(createProxyMiddleware('/gateway', { target: proxyTarget, ...proxyOptions }));
 };
