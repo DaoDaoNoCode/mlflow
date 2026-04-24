24504f64438ce4db2215d4062223d105aa73975c
Juntao Wang <juntwang@redhat.com>
2026-04-24 14:06:23 -0400

    Fix lint: replace object and empty {} types with Record<string, unknown>
    Signed-off-by: Juntao Wang <juntwang@redhat.com>

--- mlflow/server/js/src/odh/analytics/trackingProperties.ts
+++ mlflow/server/js/src/odh/analytics/trackingProperties.ts
@@ -9,15 +9,11 @@ export const enum TrackingOutcome {
   cancel = 'cancel',
 }
 
-export type BaseTrackingEventProperties = {
-  // empty for the moment
-};
-
 export type BaseFormTrackingEventProperties = {
   outcome: TrackingOutcome;
   success?: boolean;
   error?: string;
-} & BaseTrackingEventProperties;
+};
 
 export type FormTrackingEventProperties = {
   [key: string]: string | number | boolean | undefined;
@@ -30,8 +26,8 @@ export type LinkTrackingEventProperties = {
   type?: string;
   section?: string;
   name?: string;
-} & BaseTrackingEventProperties;
+};
 
 export type MiscTrackingEventProperties = {
   [key: string]: string | number | boolean | undefined;
-} & BaseTrackingEventProperties;
+};
